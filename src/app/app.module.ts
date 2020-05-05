3951c7850fe1ac87d04734f5848e558c8202d2d3
Dzianis Davydau <dzianis_davydau@epam.com>
2020-05-05 22:06:41 +0300

    feat: added first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, FirstComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ src/app/first/first.component.models.ts
@@ -0,0 +1,15 @@
+export enum Category {
+    Primary = 'Primary',
+    Secondary = 'Secondary',
+}
+
+export enum ProductColors {
+    Red = 'Red',
+    Blue = 'Blue',
+}
+
+export enum ProductSizes {
+    S = 'S',
+    M = 'M',
+    L = 'L',
+}
