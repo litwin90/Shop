a53ee195d60cc9b435150a613931edcdebd43dcf
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-05-07 15:22:34 +0300

    task1-comments

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
-import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
     declarations: [ProductComponent, ProductListComponent],
     exports: [ProductComponent, ProductListComponent],
     imports: [CommonModule, MatCardModule, MatButtonModule],
+    // мне удобно видеть эти массивы в таком порядке
+    // imports - то, что нам надо
+    // declarations - то, что тут создали
+    // exports - то, чем поделились
 })
-export class ProductsModule {}
+export class ProductsModule { }
