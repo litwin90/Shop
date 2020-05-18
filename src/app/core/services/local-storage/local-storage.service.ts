730653540a3a4ed8eb79ab56e3a84d24d26b3f55
Dzianis Davydau <dzianis_davydau@epam.com>
2020-05-18 21:39:33 +0300

    feat: implement local storage service (HomeTask3)

---
+++ src/app/core/services/local-storage/local-storage.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Inject } from '@angular/core';
+import { APP_LOCAL_STORAGE } from '../../../provider-tokens';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LocalStorageService {
+    constructor(@Inject(APP_LOCAL_STORAGE) private localStorage: Storage) {}
+
+    setItem(key: string, value: string | { [key: string]: any }) {
+        if (typeof value === 'string') {
+            this.localStorage.setItem(key, value);
+        } else {
+            this.localStorage.setItem(key, JSON.stringify(value));
+        }
+    }
+
+    getItem(key: string): string | { [key: string]: any } {
+        const storedValue = this.localStorage.getItem(key);
+        try {
+            return JSON.parse(storedValue);
+        } catch {
+            return storedValue;
+        }
+    }
+
+    removeItem(key: string) {
+        this.localStorage.removeItem(key);
+    }
+}
---
+++ src/app/provider-tokens.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from '@angular/core';
+
+export const APP_LOCAL_STORAGE = new InjectionToken<Storage>('appLocalStorage');
