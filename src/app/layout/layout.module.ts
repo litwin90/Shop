1e1d0bfb53aae0bb2628481a5a4bf8704c65382a
Dzianis Davydau <dzianis_davydau@epam.com>
2020-05-24 17:56:32 +0300

    feat: wrap all base components with BasePageComponent to have the same wrapper styles

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { PathNotFoundComponent } from './components/path-not-found/path-not-found.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
+    imports: [SharedModule],
     declarations: [AboutComponent, HomeComponent, PathNotFoundComponent],
     exports: [AboutComponent, HomeComponent, PathNotFoundComponent],
-    imports: [CommonModule],
 })
 export class LayoutModule {}
